528207c51c7f251748eaf4357a6b1ba81a45a7ab
Devon Jerothe <devonjerothe@gmail.com>
2025-08-18 13:36:35 -0400

    Initial Commit

---
+++ FleetioDemo/FleetioDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FleetioDemoApp.swift
+//  FleetioDemo
+//
+//  Created by devon jerothe on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct FleetioDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
